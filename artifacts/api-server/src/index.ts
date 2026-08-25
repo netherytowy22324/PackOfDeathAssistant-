37ca9345f63af4cec45522884f0a106a6d8c9214
netherytowy22324 <janfiutka2012@gmail.com>
2026-08-25 14:58:19 +0200

    Disable automatic Minecraft and RCON startup

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,17 +1,10 @@
 import { logger } from "./lib/logger.js";
 import { startDiscordBot } from "./services/discord-bot.js";
-import { connectMinecraft } from "./services/minecraft-bot.js";
-import { connectRcon } from "./services/rcon.js";
 import { startWatchdog } from "./services/watchdog.js";
 
 async function main(): Promise<void> {
   if (process.env["DISCORD_BOT_ENABLED"] !== "false") {
     await startDiscordBot();
-  }
-
-  if (process.env["MC_BOT_ENABLED"] === "true") {
-    connectMinecraft();
-    await connectRcon();
   }
 
   startWatchdog();
